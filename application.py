b4a3550f9ef1fa2cb8807572f50f13d8101c1e8e
Jonathan Gao <yezhong.gao@mail.utoronto.ca>
2025-03-28 01:19:41 -0400

    added f-mode avg plots

--- application.py
+++ application.py
@@ -43,6 +43,22 @@ def get_wavelength_images(wavelength):
     return organize_images(images)
 
 
+def get_average_plot_images():
+    image_dir = os.path.join(app.static_folder, 'imgs')
+    pattern = r"PA_Average_ROI_mean_(left|right)_lung_mice_[\d+_]+_(\d+)nm\.png"
+    avg_plots = {}
+
+    for img in os.listdir(image_dir):
+        match = re.match(pattern, img)
+        if match:
+            side, wavelength = match.groups()
+            if wavelength not in avg_plots:
+                avg_plots[wavelength] = {'left': None, 'right': None}
+            avg_plots[wavelength][side] = img
+    print(avg_plots)
+    return avg_plots
+
+
 @app.route('/')
 def index():
     return render_template('index.html', content='Welcome')
@@ -53,6 +69,9 @@ def f_mode(wavelength):
     if wavelength in ['750', '806', '850']:
         organized_images = get_wavelength_images(wavelength)
         return render_template('index.html', content=f'{wavelength}nm Information', organized_images=organized_images)
+    elif wavelength == 'avg_plots':
+        avg_plots = get_average_plot_images()
+        return render_template('index.html', content='Average Plots', avg_plots=avg_plots)
     else:
         return "Invalid wavelength", 400
 
